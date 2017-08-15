bfb1ce50ad719d725bb2d50498f4a454c15a8e5c
George Lemish <yorgus@yandex.ru>
2017-08-15 14:04:48 +0300

    admin breadcrumbs & alert

--- application/admin/views/chunks/alerts.php
+++ application/admin/views/chunks/alerts.php
@@ -1,22 +1,16 @@
-<?php if ($this->params['errors']) { ?>
+<?php if ($this->params['messages'] || $this->params['errors']) { ?>
     <div class="row">
-        <div class="col-md-12">
-            <?php foreach ($this->params['errors'] as $message) { ?>
-                <div class="alert alert-danger">
-                    <p><?= $message ?></p>
-                </div>
+        <div class="col-lg-12">
+            <?php if ($this->params['messages']) { ?>
+                <?php foreach ($this->params['messages'] as $message) { ?>
+                    <div class="alert alert-success"><?= $message ?></div>
+                <?php } ?>
             <?php } ?>
-        </div>
-    </div>
-<?php } ?>
 
-<?php if ($this->params['messages']) { ?>
-    <div class="row">
-        <div class="col-md-12">
-            <?php foreach ($this->params['messages'] as $message) { ?>
-                <div class="alert alert-success">
-                    <p><?= $message ?></p>
-                </div>
+            <?php if ($this->params['errors']) { ?>
+                <?php foreach ($this->params['errors'] as $message) { ?>
+                    <div class="alert alert-danger"><?= $message ?></div>
+                <?php } ?>
             <?php } ?>
         </div>
     </div>
---
+++ application/admin/views/chunks/breadcrumbs.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="col-md-12">
+        <?= \yii\widgets\Breadcrumbs::widget([
+            'homeLink' => false,
+            'links' => $links
+        ]) ?>
+    </div>
+</div>
